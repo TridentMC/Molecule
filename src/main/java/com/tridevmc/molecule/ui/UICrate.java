1e4dbd828f146454cfad089b0c38cd90b79f72c8
Benjamin K <bk1325@gmail.com>
2019-05-06 00:09:19 -0700

    Add more utility methods to IScreenContext, init elements properly. Add buttons.

--- src/main/java/com/tridevmc/molecule/ui/UICrate.java
+++ src/main/java/com/tridevmc/molecule/ui/UICrate.java
@@ -4,6 +4,7 @@ import com.tridevmc.compound.ui.Rect2D;
 import com.tridevmc.compound.ui.container.CompoundContainer;
 import com.tridevmc.compound.ui.container.CompoundUIContainer;
 import com.tridevmc.compound.ui.element.ElementBox;
+import com.tridevmc.compound.ui.element.button.ElementButton;
 import com.tridevmc.compound.ui.layout.*;
 
 public class UICrate extends CompoundUIContainer {
@@ -15,13 +16,13 @@ public class UICrate extends CompoundUIContainer {
     public void initElements() {
         ElementBox bg = new ElementBox(new Rect2D(0, 0, 178, 160), new LayoutCentered(true, true));
         this.addElement(bg);
-        LayoutGrid crateGrid = new LayoutGrid(new Rect2D(8, 8, 19*9, 18*3));
+        LayoutGrid crateGrid = new LayoutGrid(new Rect2D(8, 8, 19 * 9, 18 * 3));
         ILayout crateLayout = new LayoutMulti(crateGrid, new LayoutRelative(bg));
 
-        LayoutGrid playerGrid = new LayoutGrid(new Rect2D(8, 76, 19*9, 18*3));
+        LayoutGrid playerGrid = new LayoutGrid(new Rect2D(8, 76, 19 * 9, 18 * 3));
         ILayout playerLayout = new LayoutMulti(playerGrid, new LayoutRelative(bg));
 
-        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2D(8, 134, 19*9, 18));
+        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2D(8, 134, 19 * 9, 18));
         ILayout hotbarLayout = new LayoutMulti(hotbarGrid, new LayoutRelative(bg));
 
         for (int i = 0; i < this.inventorySlots.inventorySlots.size(); i++) {
@@ -33,5 +34,8 @@ public class UICrate extends CompoundUIContainer {
                 hotbarGrid.registerElement(this.addSlotElement(hotbarLayout, i));
             }
         }
+
+        ElementButton button = new ElementButton(new Rect2D(0, 0, 50, 50), new LayoutRelative(bg));
+        this.addElement(button);
     }
 }
