0051a660bc379cfa10a3b8bfe39ec73cf0e7b140
Benjamin K <bk1325@gmail.com>
2020-05-06 17:28:25 -0700

    Fix broken grid layout calculations

--- src/main/java/com/tridevmc/molecule/ui/UICrate.java
+++ src/main/java/com/tridevmc/molecule/ui/UICrate.java
@@ -1,7 +1,6 @@
 package com.tridevmc.molecule.ui;
 
 import com.tridevmc.compound.ui.Rect2D;
-import com.tridevmc.compound.ui.container.CompoundContainer;
 import com.tridevmc.compound.ui.container.CompoundUIContainer;
 import com.tridevmc.compound.ui.element.ElementBox;
 import com.tridevmc.compound.ui.element.ElementLabel;
@@ -19,13 +18,13 @@ public class UICrate extends CompoundUIContainer<ContainerCrate> {
     public void initElements() {
         ElementBox bg = new ElementBox(new Rect2D(0, 0, 178, 160), new LayoutCentered(true, true));
         this.addElement(bg);
-        LayoutGrid crateGrid = new LayoutGrid(new Rect2D(8, 8, 19 * 9, 18 * 3));
+        LayoutGrid crateGrid = new LayoutGrid(new Rect2D(8, 8, 18 * 9, 18 * 3));
         ILayout crateLayout = new LayoutMulti(crateGrid, new LayoutRelative(bg));
 
-        LayoutGrid playerGrid = new LayoutGrid(new Rect2D(8, 76, 19 * 9, 18 * 3));
+        LayoutGrid playerGrid = new LayoutGrid(new Rect2D(8, 76, 18 * 9, 18 * 3));
         ILayout playerLayout = new LayoutMulti(playerGrid, new LayoutRelative(bg));
 
-        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2D(8, 134, 19 * 9, 18));
+        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2D(8, 134, 18 * 9, 18));
         ILayout hotbarLayout = new LayoutMulti(hotbarGrid, new LayoutRelative(bg));
 
         for (int i = 0; i < this.getContainer().inventorySlots.size(); i++) {
@@ -39,10 +38,10 @@ public class UICrate extends CompoundUIContainer<ContainerCrate> {
         }
 
         ElementLabel label = new ElementLabel(new Rect2D(0, 1, 16, 16), new LayoutMarquee(), this.font);
-        label.setText("WELCOME TO MY SUPER COOL UI");
+        label.setText("Hey.");
         this.addElement(label);
 
-        ElementButton button = new ElementButton(new Rect2D(0, 0, 50, 50), new LayoutRelative(bg));
-        this.addElement(button);
+        //ElementButton button = new ElementButton(new Rect2D(0, 0, 50, 50), new LayoutRelative(bg));
+        //this.addElement(button);
     }
 }
