3d954aad1df76e0e4631082c6ac0fb5bf503cd3a
darkevilmac <bk1325@gmail.com>
2025-10-13 21:10:56 -0700

    Remove texture binding, implement compound renderable to pass to the gui state.

--- src/main/java/com/tridevmc/molecule/ui/CrateUI.java
+++ src/main/java/com/tridevmc/molecule/ui/CrateUI.java
@@ -3,8 +3,10 @@ package com.tridevmc.molecule.ui;
 import com.tridevmc.compound.ui.Rect2F;
 import com.tridevmc.compound.ui.container.CompoundUIContainer;
 import com.tridevmc.compound.ui.element.ElementBox;
+import com.tridevmc.compound.ui.element.ElementLabel;
 import com.tridevmc.compound.ui.element.button.ElementButton;
 import com.tridevmc.compound.ui.layout.*;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
@@ -16,15 +18,32 @@ public class CrateUI extends CompoundUIContainer<CrateMenu> {
 
     @Override
     public void initElements() {
-        ElementBox bg = new ElementBox(new Rect2F(0, 0, 178, 160), new LayoutCentered(true, true));
+        // Background box - made taller to accommodate labels
+        ElementBox bg = new ElementBox(new Rect2F(0, 0, 178, 190), new LayoutCentered(true, true));
         this.addElement(bg);
-        LayoutGrid crateGrid = new LayoutGrid(new Rect2F(8, 8, 18 * 9, 18 * 3));
+
+        // "Crate" label at the top (with shadow for visibility)
+        ElementLabel crateLabel = new ElementLabel(new Rect2F(8, 6, 162, 10), new LayoutRelative(bg),
+            Minecraft.getInstance().font, false, false, false);
+        crateLabel.setText(Component.literal("Crate"));
+        this.addElement(crateLabel);
+
+        // Crate grid - moved down to make room for label
+        LayoutGrid crateGrid = new LayoutGrid(new Rect2F(8, 18, 18 * 9, 18 * 3));
         ILayout crateLayout = new LayoutMulti(crateGrid, new LayoutRelative(bg));
 
-        LayoutGrid playerGrid = new LayoutGrid(new Rect2F(8, 76, 18 * 9, 18 * 3));
+        // "Inventory" label between crate and player inventory (with shadow for visibility)
+        ElementLabel inventoryLabel = new ElementLabel(new Rect2F(8, 80, 162, 10), new LayoutRelative(bg),
+            Minecraft.getInstance().font, false, false, false);
+        inventoryLabel.setText(Component.literal("Inventory"));
+        this.addElement(inventoryLabel);
+
+        // Player inventory grid - adjusted for new label positions
+        LayoutGrid playerGrid = new LayoutGrid(new Rect2F(8, 92, 18 * 9, 18 * 3));
         ILayout playerLayout = new LayoutMulti(playerGrid, new LayoutRelative(bg));
 
-        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2F(8, 134, 18 * 9, 18));
+        // Hotbar grid - adjusted for new label positions
+        LayoutGrid hotbarGrid = new LayoutGrid(new Rect2F(8, 154, 18 * 9, 18));
         ILayout hotbarLayout = new LayoutMulti(hotbarGrid, new LayoutRelative(bg));
 
         for (int i = 0; i < this.getMenu().slots.size(); i++) {
